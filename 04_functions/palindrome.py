adc3dc4feb50df39a320e0cda2261fd5d2cd0fe9
mar <mar.dls@tutamail.com>
2020-09-05 11:41:32 -0500

    Adding is_palindrome_list function.

--- 04_functions/palindrome.py
+++ 04_functions/palindrome.py
@@ -28,6 +28,19 @@ def is_palindrome(s):
     return is_pal(to_chars(s))
 
 
+def is_palindrome_list(l):
+    '''
+    Assumes l is a list
+    Returns True if the lsit is a palindrome; False otherwise
+    '''
+    temp = l[:]
+    temp.reverse()
+    if temp == l:
+        return True
+    else:
+        return False
+
+
 def test_is_palindrome():
     word = 'anita lava la tina'
     print('is ' +  '"' + word + '"' + ' a palindrome?')
@@ -38,5 +51,16 @@ def test_is_palindrome():
     print(is_palindrome(word))
 
 
+def test_is_palindrome_list():
+    l = ['h', 'o', 'l', 'a']
+    print('is ', l, 'a palindrome?')
+    print(is_palindrome_list(l))
+
+    l = ['a', 'b', 'b', 'a']
+    print('is ', l, 'a palindrome?')
+    print(is_palindrome_list(l))
+
 if __name__ == '__main__':
     test_is_palindrome()
+    print("")
+    test_is_palindrome_list()
